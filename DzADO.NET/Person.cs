de2124d307508cdec0825ef4b1247085fd601b0f
Жаслан Рыспеков <zhaslan_12@bk.ru>
2018-04-22 22:30:42 +0600

    jj

---
+++ DzADO.NET/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzADO.NET
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public bool IsDebtor { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
