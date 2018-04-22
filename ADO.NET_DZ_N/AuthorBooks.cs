5c8a2782226b8e31b4052daa7dfcc137673d6c7d
Жаслан Рыспеков <zhaslan_12@bk.ru>
2018-04-23 00:04:52 +0600

    Signed-off-by: Жаслан Рыспеков <zhaslan_12@bk.ru> д

---
+++ ADO.NET_DZ_N/AuthorBooks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzADO.NET
+{
+    public class AuthorBooks
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Book")]
+        public int BookId { get; set; }
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+        public virtual Book Book { get; set; }
+        public virtual Author Author { get; set; } 
+    }
+}
---
+++ ADO.NET_DZ_N/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_DZ_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FirstTask();
+            //SecondTask();
+            //ThirdTask();
+            //FourthTask();
+            FifthTask();
+            Console.ReadLine();
+        }
+        public static void FifthTask()
+        {
+            using (var context = new PersonContext())
+            {
+                var deptors = context.Persons.Where(d => d.IsDebtor == false);
+
+                foreach (var deptor in deptors)
+                {
+                    context.Entry(deptor).State = EntityState.Modified;
+                }
+                context.SaveChanges();
+            }
+        }
+        public static void FourthTask()
+        {
+            using (var context = new PersonContext())
+            {
+                var books = context.Books.Where(b => b.PersonId == 2);
+                
+                foreach(var book in books)
+                {
+                    Console.WriteLine(book.Name);
+                }
+            }
+        }
+        public static void ThirdTask()
+        {
+            using (var context = new PersonContext())
+            {
+                var books = context.Books.Where(b => b.PersonId == null);
+
+                foreach(var book in books)
+                {
+                    Console.WriteLine(book.Person.FullName);
+                }
+            }
+        }
+        public static void SecondTask()
+        {
+            using (var context = new PersonContext())
+            {
+                var authors = context.AuthorBooks.Where(c => c.BookId == 3);
+
+                foreach(var author in authors)
+                {
+                    Console.WriteLine(author.Author.Name);
+                }
+            }
+        }
+        public static void FirstTask()
+        {
+            using (var context = new PersonContext())
+            {
+                var debtors = context.Persons.Where(d => d.IsDebtor);
+
+                foreach(var debtor in debtors)
+                {
+                    Console.WriteLine(debtor.FullName);
+                }
+            }
+        }
+    }
+}
